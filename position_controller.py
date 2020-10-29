84c4b9219f38c3ac5c6f7d7ac25d1d59580eedfa
Ash-42 <56372418+Ash-42@users.noreply.github.com>
2020-10-29 19:44:09 +0530

    gps coordinates to euler angles

--- position_controller.py
+++ position_controller.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python2
 
+import math
 import time
-import tf
 import rospy
 from vitarana_drone.msg import *
 from pid_tune.msg import PidTune
 from sensor_msgs.msg import NavSatFix
 from std_msgs.msg import Float32
 
-class Cascaded_Controller():
+class ControlSystem():
 
     def __init__(self):
 
@@ -16,14 +16,14 @@ class Cascaded_Controller():
 
         # [latitude, longitude, altitude]
         self.position_gps = [0.0, 0.0, 0.0]
-        self.setpoint_gps = [0.0, 0.0, 0.0]
+        self.setpoint_gps = [19.0001, 72.0000, 1]
 
         # [roll, pitch, altitude]
         self.Kp = [0, 0, 0]
         self.Ki = [0, 0, 0]
         self.Kd = [0, 0, 0]
 
-        self.prev_errors = [0.0, 0.0, 0.0]
+        self.prev_pid_errors = [0.0, 0.0, 0.0]
         self.iTerm = [0.0, 0.0, 0.0]
         self.outputs = [0.0, 0.0, 0.0]
         self.min_value = 1000
@@ -37,10 +37,11 @@ class Cascaded_Controller():
         self.drone_cmd.rcYaw = 0.0
         self.drone_cmd.rcThrottle = 0.0
 
-        self.pwm_pub = rospy.Publisher('/drone_command', edrone_cmd, queue_size=1)
+        self.drone_pub = rospy.Publisher('/drone_command', edrone_cmd, queue_size=1)
         self.roll_error_pub = rospy.Publisher('/roll_error', Float32, queue_size=1)
         self.pitch_error_pub = rospy.Publisher('/pitch_error', Float32, queue_size=1)
-        self.alt_error_pub = rospy.Publisher('/altitude_error', Float32, queue_size=1)
+        self.z_error_pub = rospy.Publisher('/z_error', Float32, queue_size=1)
+        self.zero_error_pub = rospy.Publisher('/zero_error', Float32, queue_size=1)
 
         rospy.Subscriber('/edrone/gps', NavSatFix, self.gps_callback)
         rospy.Subscriber('/pid_tuning_roll', PidTune, self.roll_set_pid)
@@ -67,26 +68,30 @@ class Cascaded_Controller():
         self.Ki[2] = alt.Ki * 0.008
         self.Kd[2] = alt.Kd * 0.3
 
-    def transform_inputs(self):
-        self.setpoint_gps = [19.0001, 72.0000, 1]
-
-    def calculate_errors(self, errors):
+    def calculate_pos_errors(self, errors):
         errors[0] = self.setpoint_gps[0] - self.position_gps[0]
         errors[1] = self.setpoint_gps[1] - self.position_gps[1]
         errors[2] = self.setpoint_gps[2] - self.position_gps[2]
 
-        self.roll_error_pub.publish(errors[0])
-        self.pitch_error_pub.publish(errors[1])
-        self.alt_error_pub.publish(errors[2])
+    def world_to_body_conversion(self, gps_errors, pid_errors):
+        lat_error, long_error, alt_error = [*gps_errors]
+
+        tan_roll = alt_error / lat_error
+        tan_pitch = long_error / alt_error
+        tan_yaw = lat_error / long_error
+
+        pid_errors[0] = math.degrees(math.atan(tan_roll))
+        pid_errors[1] = math.degrees(math.atan(tan_pitch))
+        pid_errors[2] = math.degrees(math.atan(tan_yaw))
 
     def calculate_pid_eq(self, errors):
         pTerm = [self.Kp[0] * errors[0],
                  self.Kp[1] * errors[1],
                  self.Kp[2] * errors[2]]
 
-        dTerm = [self.Kd[0] * (errors[0] - self.prev_errors[0]),
-                 self.Kd[1] * (errors[1] - self.prev_errors[1]),
-                 self.Kd[2] * (errors[2] - self.prev_errors[2])]
+        dTerm = [self.Kd[0] * (errors[0] - self.prev_pid_errors[0]),
+                 self.Kd[1] * (errors[1] - self.prev_pid_errors[1]),
+                 self.Kd[2] * (errors[2] - self.prev_pid_errors[2])]
 
         self.iTerm[0] = self.Ki[0] * (self.iTerm[0] + errors[0])
         self.iTerm[1] = self.Ki[1] * (self.iTerm[1] + errors[1])
@@ -97,35 +102,45 @@ class Cascaded_Controller():
         self.outputs[2] = pTerm[2] + self.iTerm[2] + dTerm[2]
 
     def set_prev_vals(self, errors):
-        self.prev_errors[0] = errors[0]
-        self.prev_errors[1] = errors[1]
-        self.prev_errors[2] = errors[2]
+        self.prev_pid_errors[0] = errors[0]
+        self.prev_pid_errors[1] = errors[1]
+        self.prev_pid_errors[2] = errors[2]
 
-    def set_prop_bounds(self, euler_angle):
+    def set_cmd_bounds(self, euler_angle):
         if euler_angle > self.max_value:
             return self.max_value
         if euler_angle < self.min_value:
             return self.min_value
         return euler_angle
 
+    def set_bounds(self):
+        outputs = [self.outputs[0],
+                   self.outputs[1],
+                   self.outputs[2]]
+        [self.outputs[0],
+         self.outputs[1],
+         self.outputs[2]] = outputs = map(set_cmd_bounds, outputs)
+
     def publish_attitude(self):
         self.drone_cmd.rcRoll = self.outputs[0]
         self.drone_cmd.rcPitch = self.outputs[1]
-        # TODO
-        self.drone_cmd.rcYaw = ?
+        self.drone_cmd.rcYaw = 1500
         self.drone_cmd.rcThrottle = self.outputs[2]
+        self.drone_pub.publish(self.drone_cmd)
 
     def pid(self):
-        errors = [0.0, 0.0, 0.0]
-        self.transform_inputs()
-        self.calculate_errors(errors)
-        self.calculate_pid_eq(errors)
-        self.set_prev_vals(errors)
+        gps_errors = [0.0, 0.0, 0.0]
+        pid_errors = [0.0, 0.0, 0.0]
+        self.calculate_pos_errors(gps_errors)
+        self.world_to_body_conversion(gps_errors, pid_errors)
+        self.calculate_pid_eq(pid_errors)
+        self.set_prev_vals(pid_errors)
+        self.set_bounds()
         self.publish_attitude()
 
 if __name__ == '__main__':
-    E_DRONE = Edrone()
-    RATE = rospy.Rate(1 / E_DRONE.sample_time)
+    SYSTEM = ControlSystem()
+    RATE = rospy.Rate(1 / SYSTEM.sample_time)
     while not rospy.is_shutdown():
-        E_DRONE.pid()
+        SYSTEM.pid()
         RATE.sleep()
