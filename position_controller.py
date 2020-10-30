3b9c7bfbd78fe6077201acd853aed171eb7b8451
Ash-42 <56372418+Ash-42@users.noreply.github.com>
2020-10-30 07:48:27 +0530

    z coordinate from the /range_finder_bottom topic

--- position_controller.py
+++ position_controller.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python2
 
-import math
 import time
 import rospy
 from vitarana_drone.msg import *
 from pid_tune.msg import PidTune
-from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import NavSatFix, LaserScan
 from std_msgs.msg import Float32
 
 class ControlSystem():
@@ -15,8 +14,8 @@ class ControlSystem():
         rospy.init_node('position_controller')
 
         # [latitude, longitude, altitude]
-        self.position_gps = [0.0, 0.0, 0.0]
-        self.setpoint_gps = [25, 100, 5]
+        self.position = [0.0, 0.0, 0.0]
+        self.position_stpnt = [25, 100, 5]
 
         # [roll, pitch, altitude]
         self.Kp = [0, 0, 0]
@@ -44,14 +43,17 @@ class ControlSystem():
         self.zero_error_pub = rospy.Publisher('/zero_error', Float32, queue_size=1)
 
         rospy.Subscriber('/edrone/gps', NavSatFix, self.gps_callback)
+        rospy.Subscriber('/edrone/range_finder_bottom', LaserScan, self.range_callback)
         rospy.Subscriber('/pid_tuning_roll', PidTune, self.roll_set_pid)
         rospy.Subscriber('/pid_tuning_pitch', PidTune, self.pitch_set_pid)
         rospy.Subscriber('/pid_tuning_altitude', PidTune, self.alt_set_pid)
 
     def gps_callback(self, msg):
-        self.position_gps[0] = msg.latitude
-        self.position_gps[1] = msg.longitude
-        self.position_gps[2] = msg.altitude
+        self.position[0] = msg.latitude
+        self.position[1] = msg.longitude
+
+    def range_callback(self, msg):
+        self.position[2] = msg.ranges[0]
 
     def roll_set_pid(self, roll):
         self.Kp[0] = roll.Kp * 0.06
@@ -68,13 +70,10 @@ class ControlSystem():
         self.Ki[2] = alt.Ki * 0.008
         self.Kd[2] = alt.Kd * 0.3
 
-    def calculate_errors(self, gps_errors):
-        gps_errors[0] = self.setpoint_gps[0] - self.position_gps[0]
-        gps_errors[1] = self.setpoint_gps[1] - self.position_gps[1]
-        gps_errors[2] = self.setpoint_gps[2] - self.position_gps[2]
-
-        print 'GPS Posion: [{}, {}, {}]'.format(*self.position_gps)
-        print 'GPS Errors: [{}, {}, {}]'.format(*gps_errors)
+    def calculate_errors(self, errors):
+        errors[0] = self.position_stpnt[0] - self.position[0]
+        errors[1] = self.position_stpnt[1] - self.position[1]
+        errors[2] = self.position_stpnt[2] - self.position[2]
 
     def calculate_pid_eq(self, errors):
         pTerm = [self.Kp[0] * errors[0],
@@ -93,42 +92,46 @@ class ControlSystem():
         self.outputs[1] = pTerm[1] + self.iTerm[1] + dTerm[1]
         self.outputs[2] = pTerm[2] + self.iTerm[2] + dTerm[2]
 
-        print 'Outputs:  [{}, {}, {}]\n'.format(*self.outputs)
-
     def set_prev_vals(self, errors):
         self.prev_errors[0] = errors[0]
         self.prev_errors[1] = errors[1]
         self.prev_errors[2] = errors[2]
 
-    def set_cmd_bounds(self, euler_angle):
-        if euler_angle > self.max_value:
+    def set_cmd_bounds(self, rc_outputs):
+        if rc_outputs > self.max_value:
             return self.max_value
-        if euler_angle < self.min_value:
+        if rc_outputs < self.min_value:
             return self.min_value
-        return euler_angle
+        return rc_outputs
 
-    def set_bounds(self):
-        outputs = [self.outputs[0],
-                   self.outputs[1],
-                   self.outputs[2]]
+    def set_rc_bounds(self):
+        outputs = [i for i in self.outputs]
         [self.outputs[0],
          self.outputs[1],
-         self.outputs[2]] = outputs = map(self.set_cmd_bounds, outputs)
+         self.outputs[2]] = map(self.set_cmd_bounds, outputs)
 
-    def publish_attitude(self):
+    def publish_values(self):
         self.drone_cmd.rcRoll = self.outputs[0]
         self.drone_cmd.rcPitch = self.outputs[1]
-        self.drone_cmd.rcYaw = 1500
+        self.drone_cmd.rcYaw = 1500.0
         self.drone_cmd.rcThrottle = self.outputs[2]
+
+        self.roll_error_pub.publish(errors[0])
+        self.pitch_error_pub.publish(errors[1])
+        self.z_error_pub.publish(errors[2])
+        self.zero_error_pub.publish(0.0)
         self.drone_pub.publish(self.drone_cmd)
 
     def pid(self):
-        gps_errors = [0.0, 0.0, 0.0]
-        self.calculate_errors(gps_errors)
-        self.calculate_pid_eq(gps_errors)
-        self.set_prev_vals(gps_errors)
-        self.set_bounds()
-        self.publish_attitude()
+        errors = [0.0, 0.0, 0.0]
+        self.calculate_errors(errors)
+        self.calculate_pid_eq(errors)
+        self.set_rc_bounds()
+        self.set_prev_vals(errors)
+        self.publish_values(errors)
+        print 'Location: [{}, {}, {}]'.format(*self.position)
+        print 'Errors  : [{}, {}, {}]'.format(*errors)
+        print 'Outputs :  [{}, {}, {}]\n'.format(*self.outputs)
 
 if __name__ == '__main__':
     SYSTEM = ControlSystem()
